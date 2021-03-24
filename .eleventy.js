9d201b74180076e88250b325d2029317c3d66a2a
dylanabbott <dylanabbott@gmail.com>
2021-03-24 14:58:26 -0700

    Add RSS feed, further updates to tag functionality, add 404

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,12 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
 module.exports = (config) => {
 	config.addWatchTarget('/.src/style/style.css');
 	config.addPassthroughCopy('./src/style/style.css');
 	config.addPassthroughCopy('./src/admin');
 	config.addPassthroughCopy('./src/js');
 	config.addPassthroughCopy('./src/img');
+	config.addPlugin(pluginRss);
 	config.addCollection('tagList', require('./src/js/getTagList.js'));
 	return {
 		dir: {
